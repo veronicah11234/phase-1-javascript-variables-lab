4ef4c173ef4fcb92161529aa394d0f0be978208e
miriam <miriamwangui525@gmail.com>
2023-12-11 17:34:28 +0300

    created 1st j.s

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Code your solution in this file!
+// index.js
+
+const companyName = 'Scuber';
+let mostProfitableNeighborhood = 'Chelsea';
+let companyCeo = 'Susan Smith';
+
+// Display company information in the console
+console.log(`Company Name: ${companyName}`);
+console.log(`Most Profitable Neighborhood: ${mostProfitableNeighborhood}`);
+console.log(`CEO: ${companyCeo}`);
